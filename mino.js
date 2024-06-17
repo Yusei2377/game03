de29e337efd0f2194489d097c36c0bf6864719d2
yusei-admin <2tb7my5frk@privaterelay.appleid.com>
2024-06-17 15:47:55 +0900

    初めて

---
+++ mino.js
@@ -0,0 +1,15 @@
+const minoI = [[1,1,1,1],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+
+const minoO = [[1,1],[1,1]];
+
+const minoS = [[0,1,1],[1,1,0]];
+
+const minoZ = [[1,1,0],[0,1,1]];
+
+const minoJ = [[1,0,0],[1,1,1]];
+
+const minoL = [[0,0,1],[1,1,1]];
+
+const minoT = [[0,1,0],[1,1,1]];
+
+const minoBox = [minoI,minoO,minoS,minoZ,minoJ,minoL,minoT];
--- tetris.js
+++ tetris.js
@@ -1,76 +1,114 @@
-const canvas = document.getElementById("myCanvas");
-        const ctx = canvas.getContext("2d");
-        ctx.fillStyle = "rgb(255,255,255)";
-        ctx.strokeRect(0,0,200,400);
-        
-        const squareSize = 20;
-        ctx.lineWidth = 1;
-        for(let i = 0; i < 20; i++){  // 列
-            for(let j = 0; j < 10; j++){  // 行
-                const x = squareSize * j;
-                const y = squareSize * i;
-                // ctx.fillStyle = "rgb(0,0,0)";
-                // ctx.strokeRect(x,y,squareSize,squareSize);
+const canvas1 = document.getElementById("myCanvas1");
+const canvas2 = document.getElementById("myCanvas2");
+const canvas3 = document.getElementById("myCanvas3");
+const ctx1 = canvas1.getContext("2d");
+const ctx2 = canvas2.getContext("2d");
+const ctx3 = canvas3.getContext("2d");
+
+const squareSize = 20;
+ctx2.lineWidth = 1;
+for(let i = 0; i < 20; i++){  // 列
+    for(let j = 0; j < 10; j++){  // 行
+        const x = squareSize * j;
+        const y = squareSize * i;
+        // ctx2.fillStyle = "rgb(0,0,0)";
+        // ctx2.strokeRect(x,y,squareSize,squareSize);
+    }
+}
+
+let gameStatus = 0;
+
+const board = [
+    [0,0,0,0,0,0,0,0,0,0], //0
+    [0,0,0,0,0,0,0,0,0,0], 
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0], 
+    [0,0,0,0,0,0,0,0,0,0], //5
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0], //10
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0], //15
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0], //19
+];
+
+// const box = [1,2,3,4,5,6,7]; //確認用
+const randomBox = [];
+
+for(let i = 0; i < 7; i++){
+    const randomMino = Math.floor(Math.random() * minoBox.length);
+    const selectMino = minoBox.splice(randomMino, 1)[0];
+    randomBox.push(selectMino);
+}
+console.log(randomBox);
+
+let pickMino = randomBox[0];
+const color = "red";
+console.log(pickMino);
+for(let i = 0; i < pickMino.length; i++){ //ミノ描画
+    for(let j = 0; j < pickMino[i].length; j++){
+        if(pickMino[i][j] === 1){
+            ctx2.fillStyle = color;
+            ctx2.strokeRect
+            ctx2.fillRect(j * squareSize + squareSize, i * squareSize + squareSize, squareSize, squareSize);
+            ctx2.fillStyle = "black"
+            ctx2.strokeRect(j * squareSize + squareSize, i * squareSize + squareSize, squareSize, squareSize);
+        }
+    }
+}
+
+function rotateLeft(){
+    let baseMino = pickMino;
+    // pickMino = [];
+    let baseJ = 0;
+    // let x = 0;
+    console.log(baseMino.length);
+    // console.log("0" + x);
+    for(let i = 0; i < baseMino.length; i++){
+        console.log(baseMino[i].length);
+        for(let j = 0; j < baseMino[i].length; j++){
+            baseJ = baseMino[i].length - j-1;
+            // x = baseJ
+            // baseJ -= j;
+            pickMino[i][j] = baseMino[i][baseJ];
+            // console.log(baseMino[baseMino[i].length-baseMino[j]][i]);
+            // console.log(baseJ);
+        }
+    }
+    console.log(pickMino);
+    for(let i = 0; i < pickMino.length; i++){ //ミノ描画
+        for(let j = 0; j < pickMino[i].length; j++){
+            if(pickMino[i][j] === 1){
+                ctx2.fillStyle = color;
+                ctx2.strokeRect
+                ctx2.fillRect(j * squareSize + squareSize, i * squareSize + squareSize, squareSize, squareSize);
+                ctx2.fillStyle = "black"
+                ctx2.strokeRect(j * squareSize + squareSize, i * squareSize + squareSize, squareSize, squareSize);
+                // ctx2.fill();
             }
         }
+    }
+}
 
-        const board = [
-            [0,0,0,0,0,0,0,0,0,0], //0
-            [0,0,0,0,0,0,0,0,0,0], 
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0], 
-            [0,0,0,0,0,0,0,0,0,0], //5
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0], //10
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0], //15
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0], //19
-        ];
+addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowLeft') {
+        console.log("left");
+        rotateLeft();
+    } else if (event.key === 'ArrowRight') {
+    //   rotateRight();
+        console.log("right");
+    }
+    });
 
-        const minoI = [1,1,1,1];
-
-        const minoO = [[1,1],[1,1]];
-
-        const minoS = [[0,1,1],[1,1,0]];
-        
-        const minoZ = [[1,1,0],[0,1,1]];
-
-        const minoJ = [[1,0,0],[1,1,1]];
-
-        const minoL = [[0,0,1],[1,1,1]];
-
-        const minoT = [[0,1,0],[1,1,1]];
-
-        // const box = [minoI,minoO,minoS,minoZ,minoJ,minoL,minoT];
-        const box = [1,2,3,4,5,6,7];
-
-        // const randomMino = Math.floor(Math.random() * box.length);
-
-        let selectMino;
-
-        for(let i = 0; i < box.length; i++){
-            const randomMino = Math.floor(Math.random() * box.length);
-            selectMino = box[randomMino];
-            console.log(selectMino);
-        }
-
-        selectMino = minoT;
-        for(let i = 0; i < selectMino.length; i++){
-            for(let j = 0; j < i.length; j++){
-                if(selectMino[i][j] == 1){
-                    ctx.fillStyle = "rgb(255,0,0)";
-                    ctx.strokeRect(0 * squareSize + squareSize,0 * squareSize + squareSize,squareSize,squareSize);
-                    ctx.fillStyle();
-                }
-            }
-        }
+function start(){
+    gameStatus = 1;
+}
