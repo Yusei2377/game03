278c5865c5be880e8d3f71a37b12e86a9b672e95
yusei-admin <2tb7my5frk@privaterelay.appleid.com>
2024-06-20 11:39:39 +0900

    自由落下

--- mino.js
+++ mino.js
@@ -1,33 +1,107 @@
-const minoI = [[1,1,1,1],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+const minoI = [[1,1,1,1]];
 
-const minoO = [[1,1],[1,1],[0,0]];
+const minoO = [[1,1],[1,1]];
 
-const minoS = [[0,1,1],[1,1,0],[0,0,0]];
+const minoS = [[0,1,1],[1,1,0]];
 
-const minoZ = [[1,1,0],[0,1,1],[0,0,0]];
+const minoZ = [[1,1,0],[0,1,1]];
 
-const minoJ = [[1,0,0],[1,1,1],[0,0,0]];
+const minoJ = [[1,0,0],[1,1,1]];
 
-const minoL = [[0,0,1],[1,1,1],[0,0,0]];
+const minoL = [[0,0,1],[1,1,1]];
 
-const minoT = [[0,1,0],[1,1,1],[0,0,0]];
+const minoT = [[0,1,0],[1,1,1]];
 
-const minoBox = [minoI,minoO,minoS,minoZ,minoJ,minoL,minoT];
+let minoBox = [minoI,minoO,minoS,minoZ,minoJ,minoL,minoT];
 
-function minoColor(){
-    if(pickMino === minoI){
+function randomMino(){ //ミノをランダムにする
+    for(let i = 0; i < 7; i++){
+        const randomMino = Math.floor(Math.random() * minoBox.length);
+        const selectMino = minoBox.splice(randomMino, 1)[0];
+        randomBox.push(selectMino);
+    }
+}
+
+function pickMinoColor(){
+    if(minoKinds === minoI){
         color = "#00ffff"; //cyan
-    }else if(pickMino === minoO){
+    }else if(minoKinds === minoO){
         color = "#ffff00"; //yellow
-    }else if(pickMino === minoS){
+    }else if(minoKinds === minoS){
         color = "#00ff00"; //green
-    }else if(pickMino === minoZ){
+    }else if(minoKinds === minoZ){
         color = "#ff0000"; //red
-    }else if(pickMino === minoJ){
+    }else if(minoKinds === minoJ){
         color = "#0000ff"; //blue
-    }else if(pickMino === minoL){
+    }else if(minoKinds === minoL){
         color = "#ff8000"; //orange
-    }else if(pickMino === minoT){
+    }else if(minoKinds === minoT){
         color = "#8000ff"; //purple
+    }
+}
+
+function nextMinoColor(){
+    if(minoKinds === minoI){
+        color = "#00ffff"; //cyan
+    }else if(minoKinds === minoO){
+        color = "#ffff00"; //yellow
+    }else if(minoKinds === minoS){
+        color = "#00ff00"; //green
+    }else if(minoKinds === minoZ){
+        color = "#ff0000"; //red
+    }else if(minoKinds === minoJ){
+        color = "#0000ff"; //blue
+    }else if(minoKinds === minoL){
+        color = "#ff8000"; //orange
+    }else if(minoKinds === minoT){
+        color = "#8000ff"; //purple
+    }
+}
+
+function drawMino(){ //ミノ描画
+    clearField();
+    minoKinds = pickMino;
+    pickMinoColor();
+    for(let col = 0; col < pickMino.length; col++){
+        for(let row = 0; row < pickMino[col].length; row++){
+            let x = row * squareSize + squareSize * 3;
+            let y = col * squareSize + squareSize * fall;
+            let fx = x / 20;
+            let fy = y / 20;
+            if(pickMino[col][row] === 1){
+                field[fy][fx] = 1;
+                ctx2.fillStyle = "black"
+                ctx2.strokeRect(x, y, squareSize, squareSize);
+                ctx2.fillStyle = color;
+                // ctx2.strokeRect
+                ctx2.fillRect(x, y, squareSize, squareSize);
+                // console.log(y,x);
+                // console.log(field[fy][fx]);
+            }
+            pickMinoX = fx;
+            pickMinoY = fy;
+            // ctx2.fillStyle = "black"
+            // ctx2.strokeRect(x, y, squareSize, squareSize);
+        }
+    }
+    // console.log(field);
+    // pickMinoX = fx;
+    // pickMinoY = fy;
+    checkField();
+}
+
+function drawNextMino(nextMino,verticalPos){ //ミノ描画
+    minoKinds = nextMino;
+    nextMinoColor();
+    for(let col = 0; col < nextMino.length; col++){
+        for(let row = 0; row < nextMino[col].length; row++){
+            if(nextMino[col][row] === 1){
+                ctx3.fillStyle = color;
+                ctx3.strokeRect
+                ctx3.fillRect(row * squareSize + squareSize, col * squareSize + squareSize + (verticalPos * squareSize), squareSize, squareSize);
+                ctx3.fillStyle = "black"
+                ctx3.strokeRect(row * squareSize + squareSize, col * squareSize + squareSize + (verticalPos * squareSize), squareSize, squareSize);
+            }
+        }
     }
 }
--- tetris.js
+++ tetris.js
@@ -7,18 +7,27 @@ const ctx3 = canvas3.getContext("2d");
 
 const squareSize = 20;
 ctx2.lineWidth = 1;
-for(let i = 0; i < 20; i++){  // 列
-    for(let j = 0; j < 10; j++){  // 行
-        const x = squareSize * j;
-        const y = squareSize * i;
+for(let col = 0; col < 20; col++){  // 列
+    for(let row = 0; row < 10; row++){  // 行
+        const x = squareSize * row;
+        const y = squareSize * col;
         // ctx2.fillStyle = "rgb(0,0,0)";
         // ctx2.strokeRect(x,y,squareSize,squareSize);
     }
 }
 
 let gameStatus = 0;
+let pickMino;
+let pickMinoX,pickMinoY;
 let color;
-const board = [
+let minoKinds;
+let fall = 0;
+let time = 1000;
+
+let randomBox = [];
+let standByMinoBox = [];
+
+let field = [
     [0,0,0,0,0,0,0,0,0,0], //0
     [0,0,0,0,0,0,0,0,0,0], 
     [0,0,0,0,0,0,0,0,0,0],
@@ -41,72 +50,136 @@ const board = [
     [0,0,0,0,0,0,0,0,0,0], //19
 ];
 
-// const box = [1,2,3,4,5,6,7]; //確認用
-const randomBox = [];
-
-for(let i = 0; i < 7; i++){
-    const randomMino = Math.floor(Math.random() * minoBox.length);
-    const selectMino = minoBox.splice(randomMino, 1)[0];
-    randomBox.push(selectMino);
+function gameplay(){
+    randomMino();
+    console.log(randomBox);
+    pickMino = randomBox[0];
+    randomBox.shift();
+    // clearField();
+    drawMino();
+    standByMino();
+    // checkField();
 }
-console.log(randomBox);
-let pickMino = randomBox[0];
-minoColor();
-console.log(pickMino);
-for(let i = 0; i < pickMino.length; i++){ //ミノ描画
-    for(let j = 0; j < pickMino[i].length; j++){
-        if(pickMino[i][j] === 1){
-            ctx2.fillStyle = color;
-            ctx2.strokeRect
-            ctx2.fillRect(j * squareSize + squareSize, i * squareSize + squareSize, squareSize, squareSize);
-            ctx2.fillStyle = "black"
-            ctx2.strokeRect(j * squareSize + squareSize, i * squareSize + squareSize, squareSize, squareSize);
+
+function checkField(){
+    for(let col = 0; col < 20; col++){
+        for(let row = 0; row < 10; row++){
+            if(field[col][row] === 1){
+                // console.log("field(" + col + "," + row + ")");
+            }
+            // ctx2.fillStyle = "rgb(0,0,0))";
+            // ctx2.strokeRect(col,row,squareSize,squareSize);
+        }
+        // console.log(field);
+    }
+}
+
+function clearField(){
+    console.log("checkField");
+    for(let col = 0; col < 20; col++){
+        for(let row = 0; row < 10; row++){
+            if(field[col][row] === 1){
+                console.log(col,row);
+                ctx2.beginPath();
+                ctx2.fillStyle = "rgba(0,0,0,0)";
+                ctx2.clearRect(0,0,200,400);
+                ctx2.beginPath();
+                field[col][row] = 0;
+            }
         }
     }
 }
 
-function minoFall(){
+function checkLand(){
+    if(pickMinoY === 19){
+        
+        console.log("stop");
+        pickMino = standByMinoBox[0];
+        standByMinoBox.shift();
+    }
+}
 
+function minoFall(){
+    setInterval(function(){
+        // clearField();
+        fall += 1;
+        drawMino();
+        console.log(field);
+        // if(pickMinoY === 19){
+        //     console.log("stop");
+        //     return;
+        // }
+        checkLand();
+    },time);
+}
+
+function moveLeft(){
+    pickMino
 }
 
 // function rotateLeft(){
 //     let baseMino = pickMino;
 //     let baseJ = 0;
 //     console.log(baseMino.length);
-//     for(let i = 0; i < baseMino.length; i++){
-//         console.log(baseMino[i].length);
-//         for(let j = 0; j < baseMino[i].length; j++){
-//             baseJ = baseMino[i].length - j - 1;
-//             pickMino[i][j] = baseMino[baseJ][i];
-//             // console.log(baseMino[baseMino[i].length-baseMino[j]][i]);
-//             console.log("("+baseJ+","+i+") = "+baseMino[baseJ][i]);
+//     for(let col = 0; col < baseMino.length; col++){
+//         console.log(baseMino[col].length);
+//         for(let row = 0; row < baseMino[i].length; row++){
+//             baseJ = baseMino[col].length - row - 1;
+//             pickMino[col][row] = baseMino[row][col];
+//             // console.log(baseMino[baseMino[col].length-baseMino[row]][col]);
+//             // console.log("("+baseJ+","+col+") = "+baseMino[baseJ][col]);
 //         }
 //     }
 //     console.log(pickMino);
-//     for(let i = 0; i < pickMino.length; i++){ //ミノ描画
-//         for(let j = 0; j < pickMino[i].length; j++){
-//             if(pickMino[i][j] === 1){
-//                 ctx2.fillStyle = color;
-//                 ctx2.strokeRect
-//                 ctx2.fillRect(j * squareSize + squareSize, i * squareSize + squareSize, squareSize, squareSize);
-//                 ctx2.fillStyle = "black"
-//                 ctx2.strokeRect(j * squareSize + squareSize, i * squareSize + squareSize, squareSize, squareSize);
-//                 // ctx2.fill();
-//             }
-//         }
-//     }
+//     drawMino();
 // }
 
-addEventListener('keydown', (event) => {
-    if (event.key === 'ArrowLeft'){
-        // rotateLeft();
+function standByMino(){
+    let nextMino;
+    let verticalPos = 0;
+    for(let i = 0; i = randomBox.length; i++){
+        standByMinoBox.push = randomBox[i];
+        // nextMino = randomBox[i];
+        // drawNextMino(nextMino,verticalPos);
+        // verticalPos += nextMino.length + 1;
+        randomBox.shift();
+        // nextMino = [];
+    }
+    for(let j = 0; j = standByMinoBox.length; j++){
+        nextMino = standByMinoBox[j];
+        drawNextMino(nextMino,verticalPos);
+        verticalPos += nextMino.length + 1;
+        nextMino = [];
+    }
+    minoBox = [minoI,minoO,minoS,minoZ,minoJ,minoL,minoT];
+}
+
+addEventListener('keydown', (event) => { //キー操作
+    if(event.key === 'ArrowLeft'){
+        moveLeft();
         console.log("left");
-    } else if (event.key === 'ArrowRight'){
+    }else if(event.key === 'ArrowRight'){
     //   rotateRight();
         console.log("right");
-    }
+    }else if(event.key === 'ArrowUp'){
+    //   rotateLeft();
+        console.log("up");
+    }else if(event.key === 'ArrowDown'){
+    //   rotateRight();
+        console.log("down");
+        }
     });
 
 function start(){
+    if(gameStatus === 0){
+        checkField();
+        gameplay();
+        minoFall();
+        // checkLand();
+    }
     gameStatus = 1;
+}
+
+function reset(){
+    window.location.reload();
 }
